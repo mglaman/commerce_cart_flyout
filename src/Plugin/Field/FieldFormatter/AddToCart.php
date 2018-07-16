251d92a3877c5889f4a9efab9b77859e88300a61
Matt Glaman <nmd.matt@gmail.com>
2018-07-16 08:46:02 -0500

    Clean up for the commit.

--- src/Plugin/Field/FieldFormatter/AddToCart.php
+++ src/Plugin/Field/FieldFormatter/AddToCart.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\commerce_cart_flyout\Plugin\Field\FieldFormatter;
 
+use Drupal\commerce_product\Entity\ProductInterface;
 use Drupal\commerce_product\Entity\ProductVariationInterface;
 use Drupal\commerce_product\PreparedAttribute;
 use Drupal\commerce_product\ProductVariationAttributeMapperInterface;
+use Drupal\commerce_product\ProductVariationFieldRendererInterface;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
@@ -12,6 +14,7 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\Markup;
+use Drupal\Core\Render\RenderContext;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Template\Attribute;
@@ -21,28 +24,99 @@ use Symfony\Component\Serializer\Serializer;
 /**
  * Drop-in replacement for the default add to cart formatter.
  *
- * This is a progessively decoupled add to cart form for Commerce Product.
+ * This is a progressively decoupled add to cart form for Commerce Product.
  */
 class AddToCart extends FormatterBase implements ContainerFactoryPluginInterface {
 
   /**
+   * The variation storage.
+   *
    * @var \Drupal\commerce_product\ProductVariationStorageInterface
    */
   protected $variationStorage;
 
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
   protected $routeMatch;
 
+  /**
+   * The serialize.
+   *
+   * @var \Symfony\Component\Serializer\Serializer
+   */
   protected $serializer;
 
+  /**
+   * The attribute mapper.
+   *
+   * @var \Drupal\commerce_product\ProductVariationAttributeMapperInterface
+   */
   protected $attributeMapper;
 
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
   protected $renderer;
 
+  /**
+   * The attribute value view builder.
+   *
+   * @var \Drupal\Core\Entity\EntityViewBuilderInterface
+   */
   protected $attributeValueViewBuilder;
 
+  /**The attribute value storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
   protected $attributeValueStorage;
 
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, RouteMatchInterface $route_match, Serializer $serializer, ProductVariationAttributeMapperInterface $attribute_mapper, RendererInterface $renderer) {
+  /**
+   * The variation field renderer.
+   *
+   * @var \Drupal\commerce_product\ProductVariationFieldRendererInterface
+   */
+  protected $variationFieldRenderer;
+
+  /**
+   * Constructs a new AddToCart object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   * @param \Symfony\Component\Serializer\Serializer $serializer
+   *   The serializer.
+   * @param \Drupal\commerce_product\ProductVariationAttributeMapperInterface $attribute_mapper
+   *   The attribute mapper.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\commerce_product\ProductVariationFieldRendererInterface $variation_field_renderer
+   *   The variation field renderer.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, RouteMatchInterface $route_match, Serializer $serializer, ProductVariationAttributeMapperInterface $attribute_mapper, RendererInterface $renderer, ProductVariationFieldRendererInterface $variation_field_renderer) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
     $this->variationStorage = $entity_type_manager->getStorage('commerce_product_variation');
     $this->routeMatch = $route_match;
@@ -51,6 +125,7 @@ class AddToCart extends FormatterBase implements ContainerFactoryPluginInterface
     $this->renderer = $renderer;
     $this->attributeValueViewBuilder = $entity_type_manager->getViewBuilder('commerce_product_attribute_value');
     $this->attributeValueStorage = $entity_type_manager->getStorage('commerce_product_attribute_value');
+    $this->variationFieldRenderer = $variation_field_renderer;
   }
 
   /**
@@ -69,7 +144,8 @@ class AddToCart extends FormatterBase implements ContainerFactoryPluginInterface
       $container->get('current_route_match'),
       $container->get('serializer'),
       $container->get('commerce_product.variation_attribute_mapper'),
-      $container->get('renderer')
+      $container->get('renderer'),
+      $container->get('commerce_product.variation_field_renderer')
     );
   }
 
@@ -85,57 +161,11 @@ class AddToCart extends FormatterBase implements ContainerFactoryPluginInterface
     if (!$default_variation) {
       return [];
     }
+    $variations = $this->loadVariations($product);
+    $prepared_attributes = $this->attributeMapper->prepareAttributes($default_variation, $variations);
 
     // Fake a requirement on the current route so that our Normalizers run.
     $this->routeMatch->getRouteObject()->setRequirement('_cart_api', 'true');
-
-    /** @var \Drupal\commerce_product\Entity\ProductVariationInterface[] $variations */
-    $variations = array_reduce(
-      $this->variationStorage->loadEnabled($product),
-      function ($carry, ProductVariationInterface $variation) {
-        $carry[$variation->uuid()] = $variation;
-        return $carry;
-      }, []);
-
-
-    $prepared_attributes = $this->attributeMapper->prepareAttributes($default_variation, $variations);
-    $rendered_attributes = array_map(function (PreparedAttribute $attribute) {
-      return array_map(function ($attribute_value_id) {
-        $attribute_value = $this->attributeValueStorage->load($attribute_value_id);
-        $attribute_value_build = $this->attributeValueViewBuilder->view($attribute_value, 'add_to_cart');
-        return [
-          'output' => $this->renderer->render($attribute_value_build),
-          'attribute_value_id' => $attribute_value_id,
-        ];
-      }, array_keys($attribute->getValues()));
-    }, $this->getPreparedAttributedByElementType($prepared_attributes, 'commerce_product_rendered_attribute'));
-
-    $variation_field_renderer = \Drupal::getContainer()->get('commerce_product.variation_field_renderer');
-    $injected_variation_fields = array_map(
-      function (ProductVariationInterface $variation) use ($variation_field_renderer) {
-        return array_filter(
-          array_map(function ($build) {
-            return [
-              'class' => $build['#ajax_replace_class'],
-              'output' => trim($this->renderer->render($build)),
-            ];
-          }, $variation_field_renderer->renderFields($variation, $this->viewMode)),
-          function ($built) {
-            return !empty($built['output']);
-          });
-      }, $variations
-    );
-
-    $element_attributes = new Attribute([
-      'data-product' => $product->uuid(),
-      'data-view-mode' => $this->viewMode,
-      'data-langcode' => $langcode,
-    ]);
-
-    $add_to_cart_button = ['#theme' => 'commerce_cart_flyout_add_to_cart_button'];
-    $add_to_cart_select = ['#theme' => 'commerce_cart_flyout_add_to_cart_attributes_select'];
-    $add_to_cart_radios = ['#theme' => 'commerce_cart_flyout_add_to_cart_attributes_radios'];
-    $add_to_cart_rendered = ['#theme' => 'commerce_cart_flyout_add_to_cart_attributes_rendered'];
 
     $elements = [];
     $elements[0]['add_to_cart_form'] = [
@@ -151,24 +181,30 @@ class AddToCart extends FormatterBase implements ContainerFactoryPluginInterface
               'defaultVariation' => $default_variation->uuid(),
               'variations' => $this->serializer->normalize($variations),
               'attributes' => $this->serializer->normalize(array_values($prepared_attributes)),
-              'renderedAttributes' => $rendered_attributes,
-              'injectedFields' => $injected_variation_fields,
+              'renderedAttributes' => $this->renderPreparedAttributes($prepared_attributes),
+              'injectedFields' => $this->getVariationInjectedFields($variations),
             ],
           ],
           'theme' => [
-            'commerce_cart_flyout_add_to_cart_button' => $this->renderer->render($add_to_cart_button),
-            'commerce_cart_flyout_add_to_cart_attributes_select' => $this->renderer->render($add_to_cart_select),
-            'commerce_cart_flyout_add_to_cart_attributes_radios' => $this->renderer->render($add_to_cart_radios),
-            'commerce_cart_flyout_add_to_cart_attributes_rendered' => $this->renderer->render($add_to_cart_rendered),
+            'commerce_cart_flyout_add_to_cart_button' => $this->renderTemplate('commerce_cart_flyout_add_to_cart_button'),
+            'commerce_cart_flyout_add_to_cart_attributes_select' => $this->renderTemplate('commerce_cart_flyout_add_to_cart_attributes_select'),
+            'commerce_cart_flyout_add_to_cart_attributes_radios' => $this->renderTemplate('commerce_cart_flyout_add_to_cart_attributes_radios'),
+            'commerce_cart_flyout_add_to_cart_attributes_rendered' => $this->renderTemplate('commerce_cart_flyout_add_to_cart_attributes_rendered'),
           ],
         ],
       ],
-      '#markup' => Markup::create(sprintf('<div %s></div>', $element_attributes)),
+      '#markup' => Markup::create(sprintf('<div %s></div>', new Attribute([
+        'data-product' => $product->uuid(),
+        'data-view-mode' => $this->viewMode,
+        'data-langcode' => $langcode,
+      ]))),
     ];
 
     $cacheability = new CacheableMetadata();
     $cacheability->addCacheableDependency($product);
-    $cacheability->setCacheMaxAge(0);
+    foreach ($variations as $variation) {
+      $cacheability->addCacheableDependency($variation);
+    }
     $cacheability->applyTo($elements);
 
     return $elements;
@@ -184,10 +220,103 @@ class AddToCart extends FormatterBase implements ContainerFactoryPluginInterface
     return $has_cart && $entity_type == 'commerce_product' && $field_name == 'variations';
   }
 
-  protected function getPreparedAttributedByElementType(array $prepared_attributes, $element_type) {
+  /**
+   * Filters prepared attributes by element type.
+   *
+   * @param array $prepared_attributes
+   *   The prepared attributes.
+   * @param string $element_type
+   *   The element type to filter on.
+   *
+   * @return array
+   *   An array of filter prepared attributes.
+   */
+  protected function filterPreparedAttributedByElementType(array $prepared_attributes, $element_type) {
     return array_filter($prepared_attributes, function (PreparedAttribute $attribute) use ($element_type) {
       return $attribute->getElementType() == $element_type;
     });
+  }
+
+  /**
+   * Renders the prepared attributes.
+   *
+   * @param array $prepared_attributes
+   *   The prepared attributed.
+   *
+   * @return array
+   *   The array of rendered prepated attributes.
+   */
+  protected function renderPreparedAttributes(array $prepared_attributes) {
+    return array_map(function (PreparedAttribute $attribute) {
+      return array_map(function ($attribute_value_id) {
+        $attribute_value = $this->attributeValueStorage->load($attribute_value_id);
+        $attribute_value_build = $this->attributeValueViewBuilder->view($attribute_value, 'add_to_cart');
+        return [
+          'output' => $this->renderer->render($attribute_value_build),
+          'attribute_value_id' => $attribute_value_id,
+        ];
+      }, array_keys($attribute->getValues()));
+    }, $this->filterPreparedAttributedByElementType($prepared_attributes, 'commerce_product_rendered_attribute'));
+  }
+
+  /**
+   * Load variations for the product.
+   *
+   * @param \Drupal\commerce_product\Entity\ProductInterface $product
+   *   The product.
+   *
+   * @return \Drupal\commerce_product\Entity\ProductVariationInterface[]
+   *   The variations.
+   */
+  protected function loadVariations(ProductInterface $product) {
+    return array_reduce(
+      $this->variationStorage->loadEnabled($product),
+      function ($carry, ProductVariationInterface $variation) {
+        $carry[$variation->uuid()] = $variation;
+        return $carry;
+      }, []);
+  }
+
+  /**
+   * Renders a template.
+   *
+   * @param string $hook
+   *   The theme hook.
+   *
+   * @return string
+   *   The rendered template.
+   */
+  protected function renderTemplate($hook) {
+    return $this->renderer->executeInRenderContext(new RenderContext(), function () use ($hook) {
+      $build = ['#theme' => $hook];
+      return $this->renderer->render($build);
+    });
+  }
+
+  /**
+   * Get injected variation fields.
+   *
+   * @param \Drupal\commerce_product\Entity\ProductVariationInterface[] $variations
+   *   The variations.
+   *
+   * @return array
+   *   The array of injected variation fields.
+   */
+  protected function getVariationInjectedFields(array $variations) {
+    return array_map(
+      function (ProductVariationInterface $variation) {
+        return array_filter(
+          array_map(function ($build) {
+            return [
+              'class' => $build['#ajax_replace_class'],
+              'output' => trim($this->renderer->render($build)),
+            ];
+          }, $this->variationFieldRenderer->renderFields($variation, $this->viewMode)),
+          function ($built) {
+            return !empty($built['output']);
+          });
+      }, $variations
+    );
   }
 
 }
